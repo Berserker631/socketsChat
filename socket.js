1b08ba17d584aeb9a4d5ac359d77ea001806cb2b
Ner Aguilar <ner.aguilar06@gmail.com>
2023-08-06 23:57:11 -0400

    working on files

--- socket.js
+++ socket.js
@@ -1,35 +1,38 @@
 var express = require("express");
 var app = express();
-const cors = require('cors');
+const cors = require("cors");
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
-    cors: {
-        origin: true,
-        credentials: true,
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
 });
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => { res.send('<h1>Sockets its working</h1>') });
+app.get("/", (req, res) => {
+  res.send("<h1>Sockets its working</h1>");
+});
 
+io.on("connection", (socket) => {
+  socket.on("join", (data) => {
+    socket.join(data.Session);
+    socket.broadcast.to(data.Session).emit("user joined");
+  });
 
-io.on('connection', (socket) => {
-    socket.on('join', (data) => {
-        socket.join(data.Session);
-        socket.broadcast.to(data.Session).emit('user joined');
-    });
+  socket.on("message", (data) => {
+    io.in(data.SessionID).emit("new message", data);
+  });
 
-    socket.on('message', (data) => {
-        io.in(data.SessionID).emit('new message', data)
-    })
-
-    //disconnected
-    socket.on('disconnect', () => {
-    });
+  socket.on("file", (data) => {
+    socket.broadcast.emit("file", data);
+  });
+  //disconnected
+  socket.on("disconnect", () => {});
 });
 
 server.listen(5000, function () {
-    console.log('sockets running on port:', 5000);
+  console.log("sockets running on port:", 5000);
 });
