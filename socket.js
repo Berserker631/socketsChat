3e19b345f709ae2f8fa70140d2246d1e4090df8e
Ner Aguilar <ner.aguilar06@gmail.com>
2023-08-10 17:20:21 -0400

    new born

--- socket.js
+++ socket.js
@@ -10,8 +10,6 @@ const io = require("socket.io")(server, {
   },
 });
 
-let onlineUsers = [];
-
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -19,41 +17,17 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-socket.on("new-user-add", (newUserId) => {
-  if (!onlineUsers.some((user) => user.userId === newUserId)) {
-    onlineUsers.push({userId: newUserId, socketId: socket.id});
-  }
-  //send all active users to new user
-  io.emit("get-users", onlineUsers)
-});
-  //This socket capture when a user close the aplication
-  socket.on("disconnect", ()=>{
-    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
-    //send all online user to all users
-    io.emit("get-users",)
-  });
+  console.log('a user connected');
 
-  //When a user has been diconnected this socket emit alert to all the clients connected, and refresh the connected users array
-  socket.on("offline", ()=>{
-    //remove users from active users
-    onlineUsers = onlineUsers.filter((user)=> user.socketId !== socket.id);
-    //send all online
-    io.emit("get-users", onlineUsers)
-  });
+  //send Message to all users
+  socket.on('new message', (data)=>{
+    socket.emit('get message', data)
+  })
 
-  socket.on("join", (data) => {
-    socket.join(data.Session);
-    socket.broadcast.to(data.Session).emit("user joined");
-  });
-
-  socket.on("message", (data) => {
-    io.in(data.SessionID).emit("new message", data);
-  });
-
-  socket.on("file", (data) => {
-    socket.broadcast.emit("file", data);
-  });
-});
+  socket.on('disconnect', ()=>{
+    console.log('a user disconnected');
+  })
+})
 
 server.listen(5000, function () {
   console.log("sockets running on port:", 5000);
