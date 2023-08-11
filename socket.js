991ffc02d447ae638f3b4032ce10b68249ed534a
Ner Aguilar <ner.aguilar06@gmail.com>
2023-08-11 16:54:43 -0400

    online status

--- socket.js
+++ socket.js
@@ -10,6 +10,8 @@ const io = require("socket.io")(server, {
   },
 });
 
+let onlineUsers = [];
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -17,16 +19,45 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log('a user connected');
+  socket.on('new user', (newUser) =>{
+    //Online user status
+    if (!onlineUsers.some((user) => user.UserID === newUser)) { //if user is already in the array
+      onlineUsers.push({
+        UserID: newUser,
+        socketID: socket.id
+      });
+    }
+    //send all online users to new user
+    console.log(onlineUsers);
+    io.emit('get users', onlineUsers)
+  })
 
-  //send Message to all users
+  socket.on('disconnect', () =>{
+    onlineUsers = onlineUsers.filter((user) => user.socketID !== socket.id)
+    console.log(onlineUsers);
+    //send all online users to all users
+    io.emit('get users', onlineUsers)
+  });
+
+  // socket.on("offline", () =>{
+  //   //remove user from active users
+  //   onlineUsers = onlineUsers.filter((user) => user.socketID !== socket.id);
+  //   console.log("User is offline", onlineUsers);
+  //   //send all online users to all users
+  //   io.emit('get users', onlineUsers)
+  // })
+
+  //Join a private chat room
+  socket.on("join", (data) => {
+    socket.join(data.Session);
+    socket.broadcast.to(data.Session).emit("user joined");
+  });
+
+  //Message Sockets
   socket.on('new message', (data)=>{
-    socket.emit('get message', data)
+   socket.to(data.SessionID).emit('get message',data)
   })
 
-  socket.on('disconnect', ()=>{
-    console.log('a user disconnected');
-  })
 })
 
 server.listen(5000, function () {
